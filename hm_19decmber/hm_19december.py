d4e96a8645c357239f5c331e1aa3d71db2d06d5c
Alexanr Naumov <alexandrnaumov2911@yandex.ru>
2022-12-22 00:08:14 +0300

    домашнее задание за 19 декабря(обновлено)

--- hm_19decmber/hm_19december.py
+++ hm_19decmber/hm_19december.py
@@ -7,14 +7,17 @@ class Student(Human):
         super().__init__(name, age)
         self.year_of_study = year_of_study
         self.profile = profile
-class Teacher(Human, Student):
+class Teacher(Human):
     def __init__(self, name, age, profile, teaching_experience):
-        super().__init__(name, age, profile)
+        super().__init__(name, age)
         self.teaching_experience = teaching_experience
-class Director(Human, Teacher):
+        self.profile = profile
+class Director(Human):
     def __init__(self, name, age, profile, teaching_experience, director_experience):
-        super().__init__(name, age, profile, teaching_experience)
+        super().__init__(name, age)
         self.director_experience = director_experience
+        self.profile = profile
+        self.teaching_experience = teaching_experience
 human1 = Student('Иван', 16, 10, 'informatics')
 human2 = Teacher('Ольга', 34, 'informatics', 10)
 human3 = Director('Николай', 56, 'history', 32, 10)
