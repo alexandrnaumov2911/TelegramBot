fcec737edeed764f93c1b6300e7d37a585cc6ef7
Alexanr Naumov <alexandrnaumov2911@yandex.ru>
2023-02-16 16:55:00 +0300

    повторная сдача(2) 2 промежуточной аттестации

--- interim_certification/interim_certification2.py
+++ interim_certification/interim_certification2.py
@@ -1,23 +1,23 @@
 from utils import summ, sub, div, mult
 answer = ''
-while answer != 0:
+while answer != '0':
 
-    number1 = input('Введите первое число : ')
+    number1 = input('Введите первое число : ').strip()
     while number1.isdigit() is False:
         print('Введите число корректно!')
-        number1 = input('Введите первое число : ')
+        number1 = input('Введите первое число : ').strip()
 
 
-    operation = input('Операция? (+, -, *, /): ')
+    operation = input('Операция? (+, -, *, /): ').strip()
     while (operation != '+') and (operation != '-') and (operation != '/') and (operation != '*'):
         print('Введите операцию корректно')
-        operation = input('Операция? (+, -, *, /): ')
+        operation = input('Операция? (+, -, *, /): ').strip()
 
 
-    number2 = input('Введите второе число : ')
+    number2 = input('Введите второе число : ').strip()
     while number2.isdigit() is False:
         print('Введите число корректно!')
-        number2 = input('Введите второе число : ')
+        number2 = input('Введите второе число : ').strip()
 
     if operation == '+':
         print(summ(number1, number2))
@@ -28,9 +28,9 @@ while answer != 0:
     elif operation == '*':
         print(mult(number1, number2))
 
-    answer = input('Хотите продолжить? (1 - Да, 0 - Нет) - ')
+    answer = input('Хотите продолжить? (1 - Да, 0 - Нет) - ').strip()
     while answer != '0' and answer != '1':
         print('Введите ответ корректно! ')
-        answer = input('Хотите продолжить? !(1 - Да, 0 - Нет)! - ')
+        answer = input('Хотите продолжить? !(1 - Да, 0 - Нет)! - ').strip()
 else:
     print('Конец')
