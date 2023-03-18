2ddee8bc6a6b68695265f92a7de2216a34162cf5
Alexanr Naumov <alexandrnaumov2911@yandex.ru>
2023-03-18 23:37:47 +0300

    //

--- telegram/config.py
+++ telegram/config.py
@@ -4,6 +4,14 @@ TOKEN = os.getenv("TOKEN")
 
 
 ADMINS = (747184261,)
+NGROK = 'https://98b8-178-141-16-81.eu.ngrok.io'
+
+WEBHOOK_PATH = ''
+WEBHOOK_URL = f'{NGROK}{WEBHOOK_PATH}'
+
+# настройки веб сервера
+WEBAPP_HOST = '127.0.0.1'  # or ip
+WEBAPP_PORT = 8000
 
 if not TOKEN:
     exit('Error: no token provided')
